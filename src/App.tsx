fcd5734e6cbf2587678fe8e1dc9c0c60cf784a73
Volodymyr Savko <v.savko@dinamicka.com>
2022-07-03 17:36:05 +0300

    custom calendar algorithm

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import moment, {Moment} from 'moment';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentDate, setCurrentDate] = useState(moment(new Date))
+    const endMonthDay = moment(currentDate).endOf("month").endOf("week")
+    const startMonthDay = moment(currentDate).startOf("month").startOf("week")
+    const calendarShowingDaysArray: Moment[][] = []
+    let dateForCycle = startMonthDay.clone().subtract(1, "day")
+    while (dateForCycle.isBefore(endMonthDay, "day")) {
+        calendarShowingDaysArray.push(Array(7).fill(0).map(() => dateForCycle.add(1, "day").clone()) as unknown as Moment[])
+    }
+    console.log({calendarShowingDaysArray})
+    return (
+        <>
+
+            {
+                calendarShowingDaysArray.map(week => {
+                    return <div style={{
+                        width:"300px",
+                        display: "grid",
+                        gridTemplateColumns:"1fr 1fr 1fr 1fr 1fr 1fr 1fr"
+                    }}>{week.map((day) => {
+                        return <p style={{
+                            marginLeft: "10px",
+                            backgroundColor: +day.format("D").toString() === +new Date().getDate().toString()? "red":"transparent"
+                        }}>{day.format("D").toString()}</p>
+                    })}</div>
+                })
+            }
+            <button onClick={() => {
+                setCurrentDate(currentDate.clone().subtract(1, "month"))
+            }}>-1 month
+            </button>
+            <button onClick={() => {
+                setCurrentDate(currentDate.clone().add(1, "month"))
+            }}>+1 month
+            </button>
+
+        </>
+    );
 }
 
 export default App;
